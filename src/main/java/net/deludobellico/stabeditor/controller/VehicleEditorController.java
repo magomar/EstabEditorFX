31b7df70a79c2633763a7bf2f6cdf0e50c7199af
magomar <magomar@gmail.com>
2014-08-28 23:35:30 +0200

    adds many more fields to vehicle editor (all but the armaments)

--- src/main/java/net/deludobellico/stabeditor/controller/VehicleEditorController.java
+++ src/main/java/net/deludobellico/stabeditor/controller/VehicleEditorController.java
@@ -6,6 +6,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -20,28 +21,103 @@ import java.util.ResourceBundle;
  */
 public class VehicleEditorController implements Initializable, AssetEditorController<Vehicle> {
     @FXML
-    private TextField nameTextField;
+    private TextField maxTrenchWidth;
 
     @FXML
-    private TextArea descriptionTextArea;
+    private TextField width;
 
     @FXML
-    private TextField widthTextField;
+    private TextField length;
 
     @FXML
-    private TextField lengthTextField;
+    private TextField sideArmor;
+
+//    @FXML
+//    private StackPane vehicleEditor;
 
     @FXML
-    private TextField heightTextField;
+    private TextField fuelCapacity;
 
     @FXML
-    private ComboBox<VehicleType> vehicleTypeComboBox;
+    private TextField bulkFuel;
 
     @FXML
-    private TextField weightText;
+    private TextField reliability;
 
     @FXML
-    private TextField battleWeightTextField;
+    private TextField maxFordingDepth;
+
+    @FXML
+    private TextField crossCountryMaxSpeed;
+
+    @FXML
+    private TextField crossCountryNormalSpeed;
+
+    @FXML
+    private TextField maxGradient;
+
+    @FXML
+    private TextField battleWeight;
+
+    @FXML
+    private TextField frontArmor;
+
+    @FXML
+    private CheckBox hasOpenTop;
+
+    @FXML
+    private TextField fuelConsumptionNormalSpeed;
+
+    @FXML
+    private TextField height;
+
+    @FXML
+    private TextField rearArmor;
+
+    @FXML
+    private TextField towingCapacity;
+
+    @FXML
+    private TextField fuelConsumptionMaxSpeed;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField ronsonability;
+
+    @FXML
+    private TextArea description;
+
+    @FXML
+    private TextField payloadCapacity;
+
+    @FXML
+    private ComboBox<VehicleType> vehicleType;
+
+    @FXML
+    private TextField weight;
+
+    @FXML
+    private TextField topArmor;
+
+    @FXML
+    private CheckBox hasTurret;
+
+    @FXML
+    private TextField crewTF;
+
+    @FXML
+    private TextField roadMaxSpeedTF;
+
+    @FXML
+    private TextField roadNormalSpeedTF;
+
+    @FXML
+    private TextField personnelCapacity;
+
+    @FXML
+    private TextField takeCoverMod;
 
     private Vehicle vehicle;
     private ObservableList<VehicleType> vehicleTypeObservableList = FXCollections.observableArrayList();
@@ -51,8 +127,8 @@ public class VehicleEditorController implements Initializable, AssetEditorContro
     public void initialize(URL location, ResourceBundle resources) {
         vehicle = new Vehicle();
         vehicleTypeObservableList.addAll(VehicleType.values());
-        vehicleTypeComboBox.setItems(vehicleTypeObservableList);
-        vehicleTypeComboBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<VehicleType>() {
+        vehicleType.setItems(vehicleTypeObservableList);
+        vehicleType.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<VehicleType>() {
             @Override
             public void changed(ObservableValue<? extends VehicleType> observable, VehicleType oldValue, VehicleType newValue) {
                 if (null != newValue)
@@ -62,24 +138,33 @@ public class VehicleEditorController implements Initializable, AssetEditorContro
     }
 
     @Override
-    public void setEditable(boolean isReadOnly) {
-        if (isReadOnly) {
-            nameTextField.setEditable(false);
-            descriptionTextArea.setEditable(false);
+    public void setEditable(boolean isEditable) {
+        if (!isEditable) {
+            name.setEditable(false);
+            description.setEditable(false);
+            vehicleType.setEditable(false);
+            hasTurret.setDisable(true);
+            hasOpenTop.setDisable(true);
+            width.setEditable(false);
+            height.setEditable(false);
+            length.setEditable(false);
+            weight.setEditable(false);
+            battleWeight.setEditable(false);
+
         }
     }
 
     @Override
     public void setEstabReference(Vehicle vehicle) {
         this.vehicle = vehicle;
-        nameTextField.setText(vehicle.getName());
-        descriptionTextArea.setText(vehicle.getDescription());
-        widthTextField.setText(String.valueOf(vehicle.getSize().getWidth()));
-        heightTextField.setText(String.valueOf(vehicle.getSize().getHeight()));
-        lengthTextField.setText(String.valueOf(vehicle.getSize().getLength()));
-        weightText.setText(String.valueOf(vehicle.getSize().getWeight()));
-        battleWeightTextField.setText(String.valueOf(vehicle.getBattleWeight()));
-        vehicleTypeComboBox.getSelectionModel().select(vehicle.getType());
+        name.setText(vehicle.getName());
+        description.setText(vehicle.getDescription());
+        width.setText(String.valueOf(vehicle.getSize().getWidth()));
+        height.setText(String.valueOf(vehicle.getSize().getHeight()));
+        length.setText(String.valueOf(vehicle.getSize().getLength()));
+        weight.setText(String.valueOf(vehicle.getSize().getWeight()));
+        battleWeight.setText(String.valueOf(vehicle.getBattleWeight()));
+        vehicleType.getSelectionModel().select(vehicle.getType());
     }
 
 
