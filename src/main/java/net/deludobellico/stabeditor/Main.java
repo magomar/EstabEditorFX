9247683d7ae79c005c48fdeed634a5856721a42a
magomar <magomar@gmail.com>
2014-09-18 10:54:59 +0200

    Save/Restore last source/target estab files

--- src/main/java/net/deludobellico/stabeditor/Main.java
+++ src/main/java/net/deludobellico/stabeditor/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.util.prefs.Preferences;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -18,5 +21,71 @@ public class Main extends Application {
         primaryStage.setTitle("ESTAB Editor");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    /**
+     * Returns the source estab file preference, i.e. the estab file that was last opened
+     * as a source to copy estab data.
+     * The preference is read from the OS specific registry. If no such
+     * preference can be found, null is returned.
+     *
+     * @return
+     */
+    public File getSourceEstabFilePath() {
+        Preferences prefs = Preferences.userNodeForPackage(Main.class);
+        String sourceFilePath = prefs.get("sourceEstabFilePath", null);
+        if (sourceFilePath != null) {
+            return new File(sourceFilePath);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Sets the source estab file path of the currently loaded file. The path is persisted in
+     * the OS specific registry.
+     *
+     * @param file the file or null to remove the path
+     */
+    public void setSourceEstabFilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(Main.class);
+        if (file != null) {
+            prefs.put("sourceEstabFilePath", file.getPath());
+        } else {
+            prefs.remove("sourceEstabFilePath");
+        }
+    }
+
+    /**
+     * Returns the target estab file preference, i.e. the estab file that was last opened
+     * as a target to edit estab data.
+     * The preference is read from the OS specific registry. If no such
+     * preference can be found, null is returned.
+     *
+     * @return
+     */
+    public File getTargetEstabFilePath() {
+        Preferences prefs = Preferences.userNodeForPackage(Main.class);
+        String sourceFilePath = prefs.get("targetEstabFilePath", null);
+        if (sourceFilePath != null) {
+            return new File(sourceFilePath);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Sets the source estab file path of the currently loaded file. The path is persisted in
+     * the OS specific registry.
+     *
+     * @param file the file or null to remove the path
+     */
+    public void setTargetEstabFilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(Main.class);
+        if (file != null) {
+            prefs.put("targetEstabFilePath", file.getPath());
+        } else {
+            prefs.remove("targetEstabFilePath");
+        }
     }
 }
