20e80d01215d612e46fe01a69ef728837419c591
Heine <heine.frade@gmail.com>
2014-10-27 23:19:50 +0100

    Adds the performance tab and rearranges items

--- src/main/java/net/deludobellico/stabeditor/controller/WeaponEditorController.java
+++ src/main/java/net/deludobellico/stabeditor/controller/WeaponEditorController.java
@@ -3,20 +3,17 @@ package net.deludobellico.stabeditor.controller;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.util.converter.NumberStringConverter;
-import net.deludobellico.stabeditor.data.jaxb.PrimaryRole;
-import net.deludobellico.stabeditor.data.jaxb.Weapon;
-import net.deludobellico.stabeditor.data.jaxb.WeaponType;
-import net.deludobellico.stabeditor.data.jaxb.YesNo;
+import net.deludobellico.stabeditor.data.jaxb.*;
+import net.deludobellico.stabeditor.model.WeaponModel;
 
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 /**
@@ -25,42 +22,87 @@ import java.util.ResourceBundle;
 public class WeaponEditorController implements Initializable, AssetEditorController<Weapon> {
 
     @FXML
+    private TextField reliability;
+
+    @FXML
+    private TextArea description;
+
+    @FXML
+    private Button addRange;
+
+    @FXML
+    private TextField crew;
+
+    @FXML
+    private TextField burstRadius;
+
+    @FXML
+    private TextField fireRateNormal;
+
+    @FXML
+    private TextField load;
+
+    @FXML
+    private CheckBox singleShot;
+
+    @FXML
+    private CheckBox mustDeployToFire;
+
+    @FXML
+    private Button removeRange;
+
+    @FXML
+    private ComboBox<PrimaryRole> weaponPrimaryRole;
+
+    @FXML
+    private TextField fireRateSlow;
+
+    @FXML
+    private TextField fireRateRapid;
+
+    @FXML
+    private TextField tableRange;
+
+    @FXML
+    private TextField muzzleVelocity;
+
+    @FXML
+    private TextField shellWeight;
+
+    @FXML
+    private Label ammoName;
+
+    @FXML
+    private TextField tableAccuracy;
+
+    @FXML
+    private TextField calibre;
+
+    @FXML
     private TextField weightTextField;
 
     @FXML
     private TextField name;
 
     @FXML
-    private TextArea description;
-
-    @FXML
-    private TextField crew;
-
-    @FXML
-    private TextField reliability;
-
-    @FXML
     private ComboBox<WeaponType> weaponType;
 
     @FXML
-    private CheckBox singleShot;
+    private TextField tableArmor;
 
     @FXML
-    private ComboBox<PrimaryRole> weaponPrimaryRole;
+    private TableView<RangeItem> rangeTable;
 
     @FXML
-    private TextField calibre;
-
-    @FXML
-    private TextField muzzleVelocity;
-
-    @FXML
-    private CheckBox mustDeployToFire;
+    private TextField minRange;
 
     //TODO: add PerformanceList
-
+    @FXML
+    private ListView<FireType> performanceFireTypeList;
+    private ObservableList<FireType> fireTypeObservableList = FXCollections.observableArrayList();
 
     private Weapon weapon;
+    private Performance performance;
     private ObservableList<WeaponType> weaponTypeObservableList = FXCollections.observableArrayList();
     private ObservableList<PrimaryRole> weaponPrimaryRoleObservableList = FXCollections.observableArrayList();
 
@@ -68,12 +110,25 @@ public class WeaponEditorController implements Initializable, AssetEditorControl
     public void initialize(URL location, ResourceBundle resources) {
         weapon = new Weapon();
 
+
+        performanceFireTypeList.setItems(fireTypeObservableList);
+        performanceFireTypeList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<FireType>() {
+            @Override
+            public void changed(ObservableValue<? extends FireType> observable, FireType oldValue, FireType newValue) {
+                Performance previousPerformance = performance;
+                for (Performance p : WeaponEditorController.this.weapon.getPerformanceList().getPerformance()) {
+                    if (p.getFireType().equals(newValue)) {
+                        if(oldValue!=null && previousPerformance != null)unbindPerformanceProperties(previousPerformance);
+                        bindPerformanceProperties(p);
+                        performance = p;
+                        break;
+                    }
+                }
+            }
+        });
+
         weaponTypeObservableList.addAll(WeaponType.values());
-        weaponPrimaryRoleObservableList.addAll(PrimaryRole.values());
-
         weaponType.setItems(weaponTypeObservableList);
-        weaponPrimaryRole.setItems(weaponPrimaryRoleObservableList);
-
         weaponType.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<WeaponType>() {
             @Override
             public void changed(ObservableValue<? extends WeaponType> observable, WeaponType oldValue, WeaponType newValue) {
@@ -82,6 +137,8 @@ public class WeaponEditorController implements Initializable, AssetEditorControl
             }
         });
 
+        weaponPrimaryRoleObservableList.addAll(PrimaryRole.values());
+        weaponPrimaryRole.setItems(weaponPrimaryRoleObservableList);
         weaponPrimaryRole.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<PrimaryRole>() {
             @Override
             public void changed(ObservableValue<? extends PrimaryRole> observable, PrimaryRole oldValue, PrimaryRole newValue) {
@@ -111,6 +168,27 @@ public class WeaponEditorController implements Initializable, AssetEditorControl
 
     }
 
+    private void unbindPerformanceProperties(Performance p) {
+        minRange.textProperty().unbindBidirectional(p.minRangeProperty());
+        fireRateSlow.textProperty().unbindBidirectional(p.getRof().slowProperty());
+        fireRateNormal.textProperty().unbindBidirectional(p.getRof().normalProperty());
+        fireRateRapid.textProperty().unbindBidirectional(p.getRof().rapidProperty());
+        burstRadius.textProperty().unbindBidirectional(p.burstRadiusProperty());
+        shellWeight.textProperty().unbindBidirectional(p.shellWeightProperty());
+        load.textProperty().unbindBidirectional(p.getAmmo().loadProperty());
+    }
+
+    private void bindPerformanceProperties(Performance p) {
+        minRange.textProperty().bindBidirectional(p.minRangeProperty(), new NumberStringConverter());
+        fireRateSlow.textProperty().bindBidirectional(p.getRof().slowProperty(), new NumberStringConverter());
+        fireRateNormal.textProperty().bindBidirectional(p.getRof().normalProperty(), new NumberStringConverter());
+        fireRateRapid.textProperty().bindBidirectional(p.getRof().rapidProperty(), new NumberStringConverter());
+        burstRadius.textProperty().bindBidirectional(p.burstRadiusProperty(), new NumberStringConverter());
+        shellWeight.textProperty().bindBidirectional(p.shellWeightProperty(), new NumberStringConverter());
+        load.textProperty().bindBidirectional(p.getAmmo().loadProperty(), new NumberStringConverter());
+        ammoName.setText(p.getAmmo().getName());
+    }
+
     @Override
     public void setEditable(boolean isEditable) {
         weightTextField.setEditable(isEditable);
@@ -135,18 +213,15 @@ public class WeaponEditorController implements Initializable, AssetEditorControl
         description.textProperty().bindBidirectional(weapon.descriptionProperty());
         crew.textProperty().bindBidirectional(weapon.crewProperty(), new NumberStringConverter());
         reliability.textProperty().bindBidirectional(weapon.reliabilityProperty(), new NumberStringConverter());
+        calibre.textProperty().bindBidirectional(weapon.calibreProperty(), new NumberStringConverter());
+        muzzleVelocity.textProperty().bindBidirectional(weapon.muzzleVelocityProperty(), new NumberStringConverter());
 
-        //TODO: fix combo box property
         weaponType.getSelectionModel().select(weapon.getType());
         weaponPrimaryRole.getSelectionModel().select(weapon.getPrimaryRole());
 
-        //TODO: fix boolean properties
         singleShot.setSelected((weapon.getSingleShot().equals(YesNo.YES) ? true : false));
         mustDeployToFire.setSelected((weapon.getMustDeployToFire().equals(YesNo.YES) ? true : false));
 
-
-        calibre.textProperty().bindBidirectional(weapon.calibreProperty(), new NumberStringConverter());
-        muzzleVelocity.textProperty().bindBidirectional(weapon.muzzleVelocityProperty(), new NumberStringConverter());
     }
 
     @Override
@@ -168,7 +243,7 @@ public class WeaponEditorController implements Initializable, AssetEditorControl
 
         calibre.textProperty().unbindBidirectional(weapon.calibreProperty());
         muzzleVelocity.textProperty().unbindBidirectional(weapon.muzzleVelocityProperty());
-        
+
     }
 
     @Override
@@ -179,6 +254,11 @@ public class WeaponEditorController implements Initializable, AssetEditorControl
         if (previousWeapon.getName() != null) unbindProperties(previousWeapon);
         bindProperties(newWeapon);
 
+        fireTypeObservableList.clear();
+
+        for (Map.Entry<FireType, Boolean> entry : WeaponModel.getFireTypeMap(this.weapon).entrySet()) {
+            if(entry.getValue()) fireTypeObservableList.add(entry.getKey());
+        }
     }
 
     @Override
