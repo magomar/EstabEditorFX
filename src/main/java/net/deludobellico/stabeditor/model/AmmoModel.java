4fcc6175acab9a96feca35af17fa3e76fd39c674
magomar <magomar@gmail.com>
2014-10-28 13:57:41 +0100

    Adds class AmmoModel, complete

---
+++ src/main/java/net/deludobellico/stabeditor/model/AmmoModel.java
@@ -0,0 +1,35 @@
+package net.deludobellico.stabeditor.model;
+
+import javafx.beans.property.*;
+import net.deludobellico.stabeditor.model.temp.Ammo;
+
+/**
+ * Created by Mario on 28/10/2014.
+ */
+public class AmmoModel implements PojoJFXModel<Ammo>{
+    private final  IntegerProperty id = new SimpleIntegerProperty();
+    private final  StringProperty name = new SimpleStringProperty();
+    private final  StringProperty description = new SimpleStringProperty();
+    private final  IntegerProperty minOrderQty = new SimpleIntegerProperty();
+    private final  FloatProperty minOrderWeight = new SimpleFloatProperty();
+
+    @Override
+    public Ammo getPojo() {
+        Ammo a = new Ammo();
+        a.setId(id.get());
+        a.setName(name.get());
+        a.setDescription(description.get());
+        a.setMinOrderQty(minOrderQty.get());
+        a.setMinOrderWeight(minOrderWeight.get());
+        return null;
+    }
+
+    @Override
+    public void setPojo(Ammo pojo) {
+        id.set(pojo.getId());
+        name.set(pojo.getName());
+        description.set(pojo.getDescription());
+        minOrderQty.set(pojo.getMinOrderQty());
+        minOrderWeight.set(pojo.getMinOrderWeight());
+    }
+}
