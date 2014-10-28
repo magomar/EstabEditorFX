1209aa2b2f581dc7bc12162f9158c16d2e57c7f0
magomar <magomar@gmail.com>
2014-10-28 14:25:30 +0100

    Adds complete AmmoLoadModel

---
+++ src/main/java/net/deludobellico/stabeditor/model/AmmoLoadModel.java
@@ -0,0 +1,69 @@
+package net.deludobellico.stabeditor.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import net.deludobellico.stabeditor.data.jaxb.Ammo;
+import net.deludobellico.stabeditor.data.jaxb.AmmoLoad;
+
+/**
+ * Created by Mario on 28/10/2014.
+ */
+public class AmmoLoadModel implements PojoJFXModel<AmmoLoad> {
+    private final IntegerProperty objectId = new SimpleIntegerProperty();
+    private final StringProperty name = new SimpleStringProperty();
+    private final IntegerProperty load = new SimpleIntegerProperty();
+
+    public int getObjectId() {
+        return objectId.get();
+    }
+
+    public IntegerProperty objectIdProperty() {
+        return objectId;
+    }
+
+    public void setObjectId(int objectId) {
+        this.objectId.set(objectId);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public int getLoad() {
+        return load.get();
+    }
+
+    public IntegerProperty loadProperty() {
+        return load;
+    }
+
+    public void setLoad(int load) {
+        this.load.set(load);
+    }
+
+    @Override
+    public AmmoLoad getPojo() {
+        AmmoLoad ammoLoad = new AmmoLoad();
+        ammoLoad.setObjectId(objectId.get());
+        ammoLoad.setName(name.get());
+        ammoLoad.setLoad(load.get());
+        return ammoLoad;
+    }
+
+    @Override
+    public void setPojo(AmmoLoad pojo) {
+        objectId.set(pojo.getObjectId());
+        name.set(pojo.getName());
+        load.set(pojo.getLoad());
+    }
+}
