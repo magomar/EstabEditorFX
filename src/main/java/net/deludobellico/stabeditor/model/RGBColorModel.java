06d98ed2ce22f50d73383061ea993c4d1341f084
magomar <magomar@gmail.com>
2014-10-29 15:10:44 +0100

    Completes RGBColorModel

--- src/main/java/net/deludobellico/stabeditor/model/RGBColorModel.java
+++ src/main/java/net/deludobellico/stabeditor/model/RGBColorModel.java
@@ -12,16 +12,55 @@ public class RGBColorModel implements PojoJFXModel<RGBColor> {
     private final IntegerProperty green = new SimpleIntegerProperty();
     private final IntegerProperty blue = new SimpleIntegerProperty();
 
-    // TODO getters & setters, but first do the pojo methods
+    public int getRed() {
+        return red.get();
+    }
+
+    public IntegerProperty redProperty() {
+        return red;
+    }
+
+    public void setRed(int red) {
+        this.red.set(red);
+    }
+
+    public int getGreen() {
+        return green.get();
+    }
+
+    public IntegerProperty greenProperty() {
+        return green;
+    }
+
+    public void setGreen(int green) {
+        this.green.set(green);
+    }
+
+    public int getBlue() {
+        return blue.get();
+    }
+
+    public IntegerProperty blueProperty() {
+        return blue;
+    }
+
+    public void setBlue(int blue) {
+        this.blue.set(blue);
+    }
 
     @Override
     public RGBColor getPojo() {
-        // TODO
-        return null;
+        RGBColor rgbColor = new RGBColor();
+        rgbColor.setRed(red.getValue());
+        rgbColor.setGreen(green.getValue());
+        rgbColor.setBlue(blue.getValue());
+        return rgbColor;
     }
 
     @Override
     public void setPojo(RGBColor pojo) {
-        // TODO
+        red.set(pojo.getRed());
+        green.set(pojo.getGreen());
+        blue.set(pojo.getBlue());
     }
 }
