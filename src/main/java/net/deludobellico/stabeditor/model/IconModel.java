a24891a905d65ed4c33f43451b5f138fdf11a96c
magomar <magomar@gmail.com>
2014-10-29 15:28:01 +0100

    Completes IconModel

--- src/main/java/net/deludobellico/stabeditor/model/IconModel.java
+++ src/main/java/net/deludobellico/stabeditor/model/IconModel.java
@@ -20,16 +20,133 @@ public class IconModel implements PojoJFXModel<Icon> {
     private final  ObjectProperty<ForceSize> forceSizeIcon = new SimpleObjectProperty<>();
     private final  BooleanProperty isHq = new SimpleBooleanProperty();
 
-    // TODO getters & setters, but first do the pojo methods
-
     @Override
     public Icon getPojo() {
-        // TODO
-        return null;
+        Icon icon = new Icon();
+        icon.setBackgroundColor(backgroundColor.get().getPojo());
+        icon.setBackgroundDarkColor(backgroundDarkColor.get().getPojo());
+        icon.setBackgroundLightColor(backgroundLightColor.get().getPojo());
+        return icon;
     }
 
     @Override
     public void setPojo(Icon pojo) {
-        // TODO
+        RGBColorModel bColor = new RGBColorModel();
+        bColor.setPojo(pojo.getBackgroundColor());
+        backgroundColor.set(bColor);
+        RGBColorModel bdColor = new RGBColorModel();
+        bdColor.setPojo(pojo.getBackgroundDarkColor());
+        backgroundDarkColor.set(bdColor);
+        RGBColorModel blColor = new RGBColorModel();
+        blColor.setPojo(pojo.getBackgroundLightColor());
+        backgroundLightColor.set(blColor);
+    }
+
+    public RGBColorModel getBackgroundColor() {
+        return backgroundColor.get();
+    }
+
+    public ObjectProperty<RGBColorModel> backgroundColorProperty() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(RGBColorModel backgroundColor) {
+        this.backgroundColor.set(backgroundColor);
+    }
+
+    public RGBColorModel getBackgroundDarkColor() {
+        return backgroundDarkColor.get();
+    }
+
+    public ObjectProperty<RGBColorModel> backgroundDarkColorProperty() {
+        return backgroundDarkColor;
+    }
+
+    public void setBackgroundDarkColor(RGBColorModel backgroundDarkColor) {
+        this.backgroundDarkColor.set(backgroundDarkColor);
+    }
+
+    public RGBColorModel getBackgroundLightColor() {
+        return backgroundLightColor.get();
+    }
+
+    public ObjectProperty<RGBColorModel> backgroundLightColorProperty() {
+        return backgroundLightColor;
+    }
+
+    public void setBackgroundLightColor(RGBColorModel backgroundLightColor) {
+        this.backgroundLightColor.set(backgroundLightColor);
+    }
+
+    public RGBColorModel getDesignationColor() {
+        return designationColor.get();
+    }
+
+    public ObjectProperty<RGBColorModel> designationColorProperty() {
+        return designationColor;
+    }
+
+    public void setDesignationColor(RGBColorModel designationColor) {
+        this.designationColor.set(designationColor);
+    }
+
+    public SymbolColor getSymbolColor() {
+        return symbolColor.get();
+    }
+
+    public ObjectProperty<SymbolColor> symbolColorProperty() {
+        return symbolColor;
+    }
+
+    public void setSymbolColor(SymbolColor symbolColor) {
+        this.symbolColor.set(symbolColor);
+    }
+
+    public int getMilitarySymbol() {
+        return militarySymbol.get();
+    }
+
+    public IntegerProperty militarySymbolProperty() {
+        return militarySymbol;
+    }
+
+    public void setMilitarySymbol(int militarySymbol) {
+        this.militarySymbol.set(militarySymbol);
+    }
+
+    public PictureSymbol getPictureSymbol() {
+        return pictureSymbol.get();
+    }
+
+    public ObjectProperty<PictureSymbol> pictureSymbolProperty() {
+        return pictureSymbol;
+    }
+
+    public void setPictureSymbol(PictureSymbol pictureSymbol) {
+        this.pictureSymbol.set(pictureSymbol);
+    }
+
+    public ForceSize getForceSizeIcon() {
+        return forceSizeIcon.get();
+    }
+
+    public ObjectProperty<ForceSize> forceSizeIconProperty() {
+        return forceSizeIcon;
+    }
+
+    public void setForceSizeIcon(ForceSize forceSizeIcon) {
+        this.forceSizeIcon.set(forceSizeIcon);
+    }
+
+    public boolean getIsHq() {
+        return isHq.get();
+    }
+
+    public BooleanProperty isHqProperty() {
+        return isHq;
+    }
+
+    public void setIsHq(boolean isHq) {
+        this.isHq.set(isHq);
     }
 }
