f6c04f367b170913775ea106b52cdbac61fd33ab
magomar <magomar@gmail.com>
2014-11-03 16:39:57 +0100

    New ImageModel

---
+++ src/main/java/net/deludobellico/commandops/estabeditor/model/ImageModel.java
@@ -0,0 +1,57 @@
+package net.deludobellico.commandops.estabeditor.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import net.deludobellico.commandops.estabeditor.data.jaxb.Image;
+
+/**
+ * Created by Mario on 03/11/2014.
+ */
+public class ImageModel implements PojoJFXModel<Image> {
+    private final IntegerProperty id = new SimpleIntegerProperty();
+    private final StringProperty fileId = new SimpleStringProperty();
+
+    public ImageModel(Image image) {
+        setPojo(image);
+    }
+
+    @Override
+    public Image getPojo() {
+        Image image = new Image();
+        image.setId(id.get());
+        image.setFileId(fileId.get());
+        return image;
+    }
+
+    @Override
+    public void setPojo(Image pojo) {
+        id.set(pojo.getId());
+        fileId.set(pojo.getFileId());
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getFileId() {
+        return fileId.get();
+    }
+
+    public StringProperty fileIdProperty() {
+        return fileId;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId.set(fileId);
+    }
+}
